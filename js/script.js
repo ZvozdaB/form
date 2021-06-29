720c5b4b613dadb9932819665eaa3cebd962cb67
ZvozdaB <85256527+ZvozdaB@users.noreply.github.com>
2021-06-29 19:18:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const form__login = document.querySelector(".form__login");
+const form__email = document.querySelector(".form__email");
+const form__password = document.querySelector(".form__password");
+const error = document.querySelector(".error");
+
+form__login.addEventListener('click', validation);
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+function validation(){
+	form__email.classList.remove('error_border');
+	form__password.classList.remove('error_border');
+	
+	if(form__password.value.length < 6 && !validateEmail(form__email.value)){
+		form__password.classList.add('error_border');
+		form__email.classList.add('error_border');
+		error.classList.add('show');
+		error.innerHTML = "This is not an email<br>Password is too short";
+	} else if(!validateEmail(form__email.value)){
+		form__email.classList.add('error_border');
+		error.classList.add('show');
+		error.innerHTML = "This is not an email";
+	} else if(form__password.value.length < 6){
+		form__password.classList.add('error_border');
+		error.classList.add('show');
+		error.innerHTML = `Password is too short`;
+	}else{
+		console.log("Send to the server")
+	}
+	
+}
+
+
